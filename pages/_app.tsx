80f7732b046065082cbf867bff8ab04ef43a5eee
Vladimir Aleksic <vlada.pzs@hotmail.com>
2023-04-22 13:12:40 +0200

    some small changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import { NextIntlProvider } from 'next-intl'
 import { Analytics } from '@vercel/analytics/react'
+import { Inter } from 'next/font/google'
 
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
