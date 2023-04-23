4e23f5cacc98658fde10ea9da0c2dec0f2db95a8
Vladimir Aleksic <vlada.pzs@hotmail.com>
2023-04-23 10:48:21 +0200

    footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import logo from '../public/logo.svg'
 const Footer = () => {
   const year = new Date().getFullYear()
   return (
-    <footer className='bg-gray-900 text-gray-50 w-full flex flex-col lg:flex-row items-center justify-center lg:gap-10 gap-4 p-4 text-center'>
+    <footer className='bg-gray-900 text-gray-400 w-full flex flex-col lg:flex-row items-center justify-center lg:gap-10 gap-2 p-4 text-center text-sm'>
       <Image
         src={logo}
         alt='FagusNS logo'
